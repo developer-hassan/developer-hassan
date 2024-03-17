96b9ee61ee4e387aa11e80f06e75be81b1a2084a
Muhammad Hassan <hassanasim5567@gmail.com>
2024-03-18 00:23:08 +0500

    update about.tsx

--- src/app/(sections)/about.tsx
+++ src/app/(sections)/about.tsx
@@ -9,16 +9,34 @@ export default function About() {
     >
       <H2 className="mx-auto w-max text-foreground">About Me</H2>
       <P>
-      Welcome to my digital world! I specialize in creating smart solutions using <strong>Python</strong> nd cutting-edge tech, focusing on large language models (LLMs), and other backend applications supported by the APIs using FastAPI and Django. I can efficiently create the efficient backend/full-stack solutions with collaboration since i am a good team player.
+        Welcome to my digital world! I specialize in creating smart solutions
+        using <strong>Python</strong> nd cutting-edge tech, focusing on large
+        language models (LLMs), and other backend applications supported by the
+        APIs using FastAPI and Django. I can efficiently create the efficient
+        backend/full-stack solutions with collaboration since i am a good team
+        player.
       </P>
       <P>
-      My educational journey blends an AI and computer science Bachelor's degree from National Textile University with hands-on projects and certifications. Tech tools like LangChain, TensorFlow, FastAPI, Django, Pandas, matplotlib, NumPy, SQL, vectordatabases, and streamlit are my trusted companions.
+        My educational journey blends an AI and computer science Bachelor&apos;s
+        degree from National Textile University with hands-on projects and
+        certifications. Tech tools like LangChain, TensorFlow, FastAPI, Django,
+        Pandas, matplotlib, NumPy, SQL, vectordatabases, and streamlit are my
+        trusted companions.
       </P>
       <P>
-      Beyond development, I excel in deploying applications on GCP, Azure, AWS, and crafting Restful APIs and Websockets. Git ensures smooth collaboration through version control and continuous development.
+        Beyond development, I excel in deploying applications on GCP, Azure,
+        AWS, and crafting Restful APIs and Websockets. Git ensures smooth
+        collaboration through version control and continuous development.
       </P>
       <P>
-      In machine learning, deep learning, and LLM, I specialize in fine-tuning models on custom datasets and leverage transfer learning for tailored behaviors. Proficient in SQL and vector databases, I approach data-driven solutions holistically. Community service and mentorship are integral to my ethos, creating an environment for shared learning and growth. I'm more than a developer; a problem solver, community enthusiast, and a perpetual learner, ready to contribute to your journey of success!
+        In machine learning, deep learning, and LLM, I specialize in fine-tuning
+        models on custom datasets and leverage transfer learning for tailored
+        behaviors. Proficient in SQL and vector databases, I approach
+        data-driven solutions holistically. Community service and mentorship are
+        integral to my ethos, creating an environment for shared learning and
+        growth. I&apos;m more than a developer; a problem solver, community
+        enthusiast, and a perpetual learner, ready to contribute to your journey
+        of success!
       </P>
     </section>
   );
